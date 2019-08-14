6364235cdf59a55fc827e7b0180a66f478cc8266
tmstani23 <tmstani23@gmail.com>
2019-08-14 15:46:17 -0500

    Update cash.c
    Added denominations to an array.  Added loop that loops through the denominations.  Added condition where if first denomination goes into userPaid evenly total coins can be updated to quarters / total cents.  Began calculating remaining change

--- cash.c
+++ cash.c
@@ -6,10 +6,10 @@ float userPaid;
 int cents;
 int total = 0;
 //create an array containing these variables
-int quarters = 25;
-int dimes = 10;
-int nickels = 5;
-int pennies = 1;
+int denomArr[] = {25, 10, 5, 1};
+int denomArrLength = sizeof(denomArr)/sizeof(denomArr[0]);
+float remainingChange;
+
 
 int main(void)
 {
@@ -19,25 +19,37 @@ int main(void)
         userPaid = get_float("How much do you pay?");
         //convert userPaid to cents
         cents = round(userPaid * 100);
-
+        
         printf("User paid: %f\n", userPaid);
+        
     }
     while (userPaid < 0);
     
-    //Check how many quarters go into userPaid with no remainder
-    float change = cents / quarters; 
     
-    //create function to cycle through coin denominations
     
-        //create function to check if denomination goes into cents with no remainder
-            //If no remainder update total with amnt of quarters
+    //loop through denominations
+    for (int i = 0; i < denomArrLength; i++)
+    {
+        //if denomination goes into cents with no remainder
+        if (cents % denomArr[0] == 0) {
+            printf("No remainder\n");
+            // Check how many quarters go into userPaid with no remainder
+            // If no remainder update total with amnt of quarters
+            total = cents / denomArr[0];
+        }
         
-        //create function to subtract out remaining change
+            //float change = cents / quarters; 
+            
+        
+        // subtract out remaining change
+        remainingChange = cents / denomArr[i];
+        printf("remaining change: %f\n", remainingChange);
             //Update remaining change variable
    
          //repeat steps for each denomination
-   
-    
-    
-    printf("change: %f\n", change);
+        
+        
+    }
+        
+    printf("total coins needed: %i\n", total);
 }
